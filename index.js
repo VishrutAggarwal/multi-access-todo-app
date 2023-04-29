d0ca31734a27b3de235febc1eee0b54ec8e9e4d6
Vishrut Aggarwal <vishrut.agg@gmail.com>
2023-04-29 15:13:59 +0530

    chore: set up server

---
+++ index.js
@@ -0,0 +1,20 @@
+// import necessary modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// import controllers
+const authController = require('./controllers/auth.controller');
+const todoController = require('./controllers/todo.controller');
+
+// import middleware
+const { authenticateToken } = require('./middleware/auth.middleware');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+  });
